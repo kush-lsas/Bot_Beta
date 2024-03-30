029b433a77c84794086c21c98897bb965362e367
kush-lsas <kushagra@lsas-tec.com>
2024-03-29 17:50:31 -0700

    V2

--- script.py
+++ script.py
@@ -3,47 +3,24 @@ import streamlit as st
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain.prompts import ChatPromptTemplate
-from langchain_community.document_transformers import BeautifulSoupTransformer
-from langchain_community.document_loaders import TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_community.document_transformers import BeautifulSoupTransformer
+
 from langchain_core.runnables import RunnableParallel, RunnablePassthrough
 from langchain_pinecone import PineconeVectorStore
 
 st.set_page_config(page_title="Interweb Explorer", page_icon="🌐")
 
-OPENAI_API_KEY = st.sidebar.text_input('OpenAI API Key', type='password')
-os.environ["PINECONE_API_KEY"] = "ef5d4ce9-1906-4d38-a83d-dbf18b753473"
-
-def extract_and_split_html():
-    extract_path = "Web_Pages"
-    bs_transformer = BeautifulSoupTransformer()
-
-    html_files_split = []
-
-    for filename in os.listdir(extract_path):
-        filepath = os.path.join(extract_path, filename)
-        html_loader = TextLoader(filepath)
-        doc = html_loader.load()
-        doc_transformed = bs_transformer.transform_documents(doc, tags_to_extract=["p","title", "h1", "h2", "h3", "th", "td"])
-        splitter = RecursiveCharacterTextSplitter(chunk_size = 2000, chunk_overlap = 20)
-        split_docs = splitter.split_documents(doc_transformed)
-        for splits in split_docs:
-            html_files_split.append(splits)
-          
-    return html_files_split
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 
 def generate_embeddings():
-    #html_files_split = extract_and_split_html()
     embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
     index_name = "uma-bot"
     pcstore = PineconeVectorStore.from_existing_index(index_name, embeddings)
-    #pcstore = PineconeVectorStore.from_documents(html_files_split, embeddings, index_name=index_name)
     return pcstore.as_retriever()
 
 def setup_chain():
-    model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-4-turbo-preview")
+    model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model="gpt-4-turbo-preview", streaming=True)
     parser = StrOutputParser()
     template = """
                 You are an AI assistant helping users with STK Connect commands for various use-cases. Your goal is to provide accurate, concise, and relevant answers based on the context provided.
@@ -70,13 +47,11 @@ st.sidebar.image("logo.webp")
 st.header("`UMA Bot (Connect Commands) Beta`")
 st.info("`I am an AI that can answer questions based on Connect commands that are used in STK. I can help you with the usage of Connect commands for different use-cases. Feel free to ask me anything!`")
 
-#question = st.text_input("`Ask a question:`")
-
-    # Write answer and sources
-    #retrieval_streamer_cb = PrintRetrievalHandler(st.container())
 with st.form('my_form'):
     text = st.text_area('Enter question:', 'Type here...')
+
     submitted = st.form_submit_button('Submit')
+
     if submitted:
         chain = setup_chain()
-        st.info(chain.invoke(text))
+        st.write_stream(chain.stream(text))
